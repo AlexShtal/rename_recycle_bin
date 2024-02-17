60b3f54e74b45accd4c5ed5a90be5d8763449b51
AlexShtal <aleks2015.773@gmail.com>
2024-02-18 00:46:11 +0600

    rename to c

--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 int main(){
     HKEY recycleBinHKey;
     LPCSTR registrySubkey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\CLSID\\{645FF040-5081-101B-9F08-00AA002F954E}";
-    const BYTE * pnewValue = (const BYTE *) "—Ë·¿ƒ»";
-    int newValueLen = 15;
+    const BYTE * pnewValue = (const BYTE *) "NewName";
+    int newValueLen = 8;
 
     RegOpenKeyExA(HKEY_CURRENT_USER, registrySubkey, 0, KEY_SET_VALUE, &recycleBinHKey);
     RegSetValueExA(recycleBinHKey, NULL, 0, REG_SZ, pnewValue, newValueLen);
     RegCloseKey(recycleBinHKey);
+    return 0;
 }
