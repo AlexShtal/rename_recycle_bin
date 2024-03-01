061d3bdc6f718821a47585d8eed2c39bce0a9b7f
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-03-01 18:24:03 +0600

    Heavy refactoring of main code
    List of disadvantages
    
    1. Code maintainability was poor because all parts of code are put in
       single function, code has been split into four functions
    
    2. The "const char *command[345]" and "const char *iconPath[256]"
       variables are arrays of "const char *" pointers, not arrays of chars
    
    3. Path to executable in startup is rebuilded twice in two sprintfs

--- main.c
+++ main.c
@@ -3,7 +3,27 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
+#include "resource.h"
+
+char startup_path[MAX_PATH];
+char filename[MAX_PATH];
+
+void rename_trash(void);
+void get_startup_path(void);
+void copy_to_startup(void);
+void swap_icon(void);
+
+int main(int argc, char* argv[]) {
+    rename_trash();
+    get_startup_path();
+    copy_to_startup();
+    swap_icon();
+
+    return 0;
+}
+
+void rename_trash(void)
+{
     HKEY recycleBinHKey;
     LPCSTR registrySubkey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\CLSID\\{645FF040-5081-101B-9F08-00AA002F954E}";
     const BYTE * pnewValue = (const BYTE *) "SIBADI";
@@ -11,28 +31,47 @@ int main(int argc, char* argv[]){
     RegSetValueExA(recycleBinHKey, NULL, 0, REG_SZ, pnewValue, strlen(pnewValue));
     SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_FLUSHNOWAIT, NULL, NULL);
     RegCloseKey(recycleBinHKey);
+}
 
-    const char* command[345];
-    char filename[MAX_PATH];
+void get_startup_path(void)
+{
+    char *exename;
+
     GetModuleFileName(NULL, filename, MAX_PATH);
-    char* exename = strrchr(filename, '\\');
+    exename = strrchr(filename, '\\');
+
     if (exename != NULL) {
         exename++;
     } else {
         exename = filename;
     }
 
-    sprintf(command, "copy \"%s\" \"%%USERPROFILE%%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\"", exename);
+    sprintf(startup_path, "%%USERPROFILE%%\\"
+            "AppData\\Roaming\\"
+            "Microsoft\\Windows\\"
+            "Start Menu\\Programs\\Startup\\%s",
+            exename);
+}
+
+void copy_to_startup(void)
+{
+    char command[345];
+
+    sprintf(command, "copy \"%s\" \"%s\"", filename, startup_path);
     system(command);
+}
+
+void swap_icon(void)
+{
     LPCSTR iconRegistryKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\CLSID\\{645FF040-5081-101B-9F08-00AA002F954E}\\DefaultIcon";
-    const char* iconPath[256];
+    char iconPath[256];
     HKEY recycleBinIconHKey;
-    sprintf(iconPath, "%%USERPROFILE%%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\%s,-2", exename);
+
+    sprintf(iconPath, "%s,-%d", startup_path, IDI_REPLACEMENT);
     RegOpenKeyExA(HKEY_CURRENT_USER, iconRegistryKey, 0, KEY_SET_VALUE, &recycleBinIconHKey);
     RegSetValueExA(recycleBinIconHKey, "full", 0, REG_EXPAND_SZ, (const BYTE*)iconPath, strlen(iconPath));
     RegSetValueExA(recycleBinIconHKey, "empty", 0, REG_EXPAND_SZ, (const BYTE*)iconPath, strlen(iconPath));
     RegSetValueExA(recycleBinIconHKey, NULL, 0, REG_EXPAND_SZ, (const BYTE*)iconPath, strlen(iconPath));
     RegCloseKey(recycleBinIconHKey);
     SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_FLUSHNOWAIT, NULL, NULL);
-    return 0;
 }
